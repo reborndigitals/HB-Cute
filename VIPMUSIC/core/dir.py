dbf52c60703fefdbf1eab41eafecb109c2788e42
reborndigitals <reborndigitals@gmail.com>
2024-09-20 15:15:28 +0530

    Update dir.py

--- VIPMUSIC/core/dir.py
+++ VIPMUSIC/core/dir.py
@@ -1,19 +1,53 @@
+#
+# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+#
+# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# and is released under the "MIT License".
+# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+import logging
 import os
+import sys
+from os import listdir, mkdir
 
-from ..logging import LOGGER
+from config import TEMP_DB_FOLDER
+
 
 def dirr():
+    assets_folder = "assets"
+    downloads_folder = "downloads"
+    cache_folder = "cache"
+
+    if assets_folder not in listdir():
+        logging.warning(
+            f"{assets_folder} Folder not Found. Please clone or fork repository again."
+        )
+        sys.exit()
+
     for file in os.listdir():
-        if file.endswith(".jpg"):
-            os.remove(file)
-        elif file.endswith(".jpeg"):
-            os.remove(file)
-        elif file.endswith(".png"):
+        if (
+            file.endswith(".jpg")
+            or file.endswith(".jpeg")
+            or file.endswith(".mp3")
+            or file.endswith(".png")
+            or file.endswith(".session")
+            or file.endswith(".session-journal")
+        ):
             os.remove(file)
 
-    if "downloads" not in os.listdir():
-        os.mkdir("downloads")
-    if "cache" not in os.listdir():
-        os.mkdir("cache")
+    if downloads_folder not in listdir():
+        mkdir(downloads_folder)
 
-    LOGGER(__name__).info("Directories Updated.")
+    if cache_folder not in listdir():
+        mkdir(cache_folder)
+
+    if TEMP_DB_FOLDER not in listdir():
+        mkdir(TEMP_DB_FOLDER)
+
+    logging.info("Directories Updated.")
+
+
+if __name__ == "__main__":
+    dirr()
