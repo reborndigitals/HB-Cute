99708c2339b74f0e331834ba855fe68e025f21f8
reborndigitals <reborndigitals@gmail.com>
2025-10-31 14:25:56 +0530

    config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID","8045459"))
 API_HASH = getenv("API_HASH", "e6d1f09120e17a4372fe022dde88511b")
-BOT_TOKEN = getenv("BOT_TOKEN", "2096983652:AAH9foLjRAITRRdylkP5hi31gCnE1Hzi44w")
+BOT_TOKEN = getenv("BOT_TOKEN", "8244250546:AAGv_n9GxRhZuGvzgVhEr7G_XfL7tqL8IIE")
 
 # Get your mongo url from cloud.mongodb.com
 OWNER_USERNAME = getenv("OWNER_USERNAME","rajeshrakis")
